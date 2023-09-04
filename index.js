aa53065e87f85d74f4278cdca4b2f5e0cde61ab4
Bartłomiej Szlachta <bartlomiej.szlachta@softiq.pl>
2023-09-04 13:25:21 +0200

    inne projekty

--- index.js
+++ index.js
@@ -7,46 +7,25 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use((req, res, next) => setTimeout(next, 1000 * Math.random()));
-app.use(cors())
+app.use(cors());
 
 app.get('/projects', (req, res) => {
   res.send([
     {
-      name: 'Facebook',
-      description: 'Serwis społecznościowy, w ramach którego zarejestrowani użytkownicy mogą tworzyć sieci i grupy,' +
-        ' dzielić się wiadomościami i zdjęciami oraz korzystać z aplikacji,' +
-        ' będący własnością Meta Platforms z siedzibą w Menlo Park.' +
-        ' Został uruchomiony 4 lutego 2004 na Uniwersytecie Harvarda i był początkowo przeznaczony przede wszystkim dla osób' +
-        ' studiujących na tej uczelni.',
+      name: 'Aplikacja do zarządzania zadaniami "TaskMaster"',
+      description: '"TaskMaster" to kompleksowa aplikacja webowa, której celem jest ułatwienie użytkownikom zarządzania swoimi codziennymi zadaniami. Aplikacja pozwala użytkownikom tworzyć listy zadań, nadawać im priorytety, dodawać terminy i opisy. Dodatkowo, posiada wbudowany kalendarz umożliwiający planowanie i śledzenie zadań w oparciu o daty. Użytkownicy otrzymują również powiadomienia e-mailowe lub powiadomienia push, aby nie przegapić terminów. "TaskMaster" jest intuicyjny w obsłudze i pomaga w efektywnym zarządzaniu czasem.',
     },
     {
-      name: 'Instagram',
-      description: 'Fotograficzny serwis społecznościowy hostingu zdjęć, połączony z aplikacją o tej samej nazwie' +
-        ' (dostępną na systemy operacyjne iOS i Android), który umożliwia użytkownikom edycję zdjęć i filmów,' +
-        ' stosowanie do nich filtrów cyfrowych oraz udostępnianie ich w różnych serwisach społecznościowych.' +
-        ' Charakterystyczną cechą aplikacji był nadawany zdjęciom kwadratowy kształt,' +
-        ' podobnie jak w aparatach fotograficznych marki Kodak serii Instamatic,' +
-        ' aparatach do fotografii błyskawicznej firmy Polaroid oraz w średnioformatowych aparatach formatu 6x6' +
-        ' w przeciwieństwie do proporcji obrazu 4:3,' +
-        ' który jest wykorzystywany przez większość aparatów fotograficznych oraz urządzeń mobilnych' +
-        ' dysponujących funkcją foto. 27 sierpnia 2015 roku udostępniono możliwość umieszczania zdjęć i filmów' +
-        ' w innych formatach obrazu.',
+      name: 'System zarządzania sklepem internetowym "E-CommercePro"',
+      description: '"E-CommercePro" to zaawansowany system do zarządzania sklepem internetowym, który umożliwia przedsiębiorcom prowadzenie swojego biznesu online. System ten pozwala na łatwe zarządzanie katalogiem produktów, tworzenie stron produktów, obsługę koszyka zakupowego, integrację z różnymi bramkami płatności, a także śledzenie zamówień i zarządzanie zapasami. Klienci mogą tworzyć konta, śledzić swoje zamówienia i korzystać z różnych opcji płatności. "E-CommercePro" zapewnia również narzędzia do analizy danych i monitorowania wydajności sklepu.',
     },
     {
-      name: 'WhatsApp',
-      description: 'Mobilna aplikacja dla smartfonów, służąca jako komunikator internetowy.' +
-        ' Aplikacja ta jest dostępna dla różnych platform: iOS, Android i KaiOS.' +
-        ' Dzięki tej aplikacji można przesyłać wiadomości i pliki multimedialne pomiędzy dwoma telefonami komórkowymi,' +
-        ' połączonymi z Internetem. Wymagana jest instalacja aplikacji na telefonie.' +
-        ' Możliwe jest tworzenie czatów grupowych, przesyłanie własnej pozycji dzięki Mapom Google' +
-        ' i współdzielenie kontaktów własnej rubryki. Aplikacja pozwala również na rozmowy wideo oraz VoIP.',
+      name: 'Aplikacja do analizy danych finansowych "FinAnalyze"',
+      description: '"FinAnalyze" to zaawansowana aplikacja desktopowa, stworzona z myślą o analitykach finansowych i osobach zajmujących się inwestycjami. Aplikacja umożliwia importowanie danych finansowych z różnych źródeł, takich jak giełdowe notowania, bilanse spółek czy kursy walut. Następnie użytkownicy mogą tworzyć różnorodne wykresy, raporty i analizy, które pomagają w podejmowaniu decyzji inwestycyjnych. "FinAnalyze" oferuje również zaawansowane narzędzia do prognozowania trendów ekonomicznych i ryzyka inwestycyjnego.',
     },
     {
-      name: 'Oculus',
-      description: 'Gogle wirtualnej rzeczywistości stworzone przez firmę Reality Labs (wcześniej Oculus)' +
-        ' wydane 26 marca 2016 roku. W roku 2012 firma Oculus zorganizowała zbiórkę pieniędzy na serwisie Kickstarter,' +
-        ' która ostatecznie zebrała 2,437,429 USD.' +
-        ' Popularność projektu przyczyniła się do zakupu firmy przez przedsiębiorstwo Facebook.',
+      name: 'Gra platformowa "PixelQuest"',
+      description: '"PixelQuest" to klasyczna gra platformowa inspirowana estetyką retro gier z lat 80. i 90. Gracz wciela się w postać bohatera, który musi przejść przez różnorodne poziomy pełne przeszkód, pułapek i wrogów. Celem gry jest zdobywanie punktów, zbieranie nagród i pokonywanie bossów. "PixelQuest" oferuje rozbudowany świat gry, wiele poziomów trudności i możliwość zdobywania osiągnięć. To projekt programistyczny, który łączy w sobie umiejętności programowania gier, projektowania graficznego i rozwoju interakcji użytkownika.',
     },
   ]);
 });
